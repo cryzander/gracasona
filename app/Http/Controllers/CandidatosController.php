fc2de4609e171c947b73480585bae433c449db1b
Luis Gustavo <luis.sales@iff.edu.br>
2017-02-24 00:28:51 -0300

    Parte de inserção de candidatos 1

--- app/Http/Controllers/CandidatosController.php
+++ app/Http/Controllers/CandidatosController.php
@@ -16,7 +16,24 @@ class CandidatosController extends Controller
 	{
 		$this->candidato = $candidato;
 		$this->voto = $voto;
-	}	
+	}
+
+	public function inserir($nome){
+
+
+		$insert = $this->candidato->create([
+            "name" => $nome
+        ]);
+
+
+		if ($insert){
+			return ["mensagem" => "Cadastrado com sucesso!"];
+		} else {
+			return ["mensagem" => "Ocorreu alguma falha no cadastro desta pessoa.
+			Verifique se o campo não está vazio ou não é menor que 3 caracteres"];
+		}
+
+	}
 
 	public function getAllCandidatos()
 	{
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('/candidatos', 'CandidatosController@getAllCandidatos');
 
+Route::get('/inserir/{$nome}', 'CandidatosController@inserir');
+
