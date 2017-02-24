d5b75d8eb24dc1935eeda5d60b37b29395a68d93
Luis Gustavo <luis.sales@iff.edu.br>
2017-02-24 12:37:39 -0300

    Escolher votar e votar3.

--- app/Models/Voto.php
+++ app/Models/Voto.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Voto extends Model
 {
     //
+	public $timestamps = false;
+    protected $fillable = ['id_candidato', 'sessao', 'estrelas', 'id_usuario'];
 	public $rules = [
 		'id_candidato' => 'required|numeric',
 		'sessao' => 'required|numeric',
