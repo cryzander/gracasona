34c0fbac8011232e8648972b6b81d69372e2f7d8
Luis Gustavo <luis.sales@iff.edu.br>
2017-02-24 13:45:49 -0300

    sendo votado 6.

--- app/Models/SendoVotado.php
+++ app/Models/SendoVotado.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SendoVotado extends Model
 {
     protected $fillable = ['id_candidato', 'sessao'];
+    public $timestamps = false;
 }
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     //
+	public $timestamps = false;
 	public $rules = [
 		'numerousuario' => 'required|numeric'
 	];
