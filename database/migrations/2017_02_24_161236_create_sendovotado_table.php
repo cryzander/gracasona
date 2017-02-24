ef0d753953c977af4d3e2e2531f4355747d463c2
Luis Gustavo <luis.sales@iff.edu.br>
2017-02-24 16:17:38 +0000

    Sendo votado 1.

---
+++ database/migrations/2017_02_24_161236_create_sendovotado_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSendovotadoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sendovotados', function(Blueprint $table){
+		$table->increments('id');
+		$table->integer('id_candidato');
+		$table->integer('sessao')->nullable();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sendovotados');
+    }
+}
