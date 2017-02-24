e64f9732f1b22ed86f0326dc4f63109feaafcae0
Luis Gustavo <luis.sales@iff.edu.br>
2017-02-24 00:48:45 -0300

    Resolvendo rota do inserir - agora sem timestamp

--- app/Models/Candidato.php
+++ app/Models/Candidato.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Candidato extends Model
 {
+    public $timestamps = false;
     protected $fillable = ['name'];
     public $rules = [ 'name' => 'required|min:3|max:99'];
 }
